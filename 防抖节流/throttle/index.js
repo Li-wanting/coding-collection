69edea526a67c1e095e3891c5b462b9d2f338ceb
liwanting <liwanting0813@gmail.com>
2025-07-29 11:31:22 +0800

    节流实现时间戳版

--- 防抖节流/throttle/index.js
+++ 防抖节流/throttle/index.js
@@ -10,3 +10,14 @@ const throttle = (func, interval) => {
   };
 };
 
+// 时间戳版本节流
+const throttle2 = (func, interval) => {
+  let lastTime = 0;
+  return function (...args) {
+    const now = Date.now();
+    if (now - lastTime >= interval) {
+      func && func.apply(this, args);
+      lastTime = now;
+    }
+  };
+};
